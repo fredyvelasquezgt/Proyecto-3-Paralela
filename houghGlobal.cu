f9b41934139481cb0043aa7a8a2ac681c8250158
Fredy Velásquez <59968764+fredyvelasquezgt@users.noreply.github.com>
2023-11-10 12:13:51 -0600

    Update and rename hough.cu to houghGlobal.cu

--- houghGlobal.cu
+++ houghGlobal.cu
@@ -1,11 +1,7 @@
 /*
  ============================================================================
  Author        : G. Barlas
- Version       : 1.0
- Last modified : December 2014
- License       : Released under the GNU GPL 3.0
- Description   :
- To build use  : make hough
+ To build use  : make global
  ============================================================================
  */
 #include <stdio.h>
@@ -13,34 +9,31 @@
 #include <math.h>
 #include <cuda.h>
 #include <string.h>
-#include <vector>
-#include <utility>
 #include "common/pgm.h"
-
 
 const int degreeInc = 2;
 const int degreeBins = 180 / degreeInc;
 const int rBins = 100;
 const float radInc = degreeInc * M_PI / 180;
 
-void calculateSDyMEAN(int data[], int n, float *mean, float *stddev) {
-  float sum = 0.0;
-  int i;
 
-  for(i = 0; i < n; ++i) {
-    sum += data[i];
-  }
+//************************************************************************
+// Check return function 
+//************************************************************************
 
-  *mean = sum / n;
-
-  sum = 0.0;
-
-  for(i = 0; i < n; ++i) {
-    sum += pow(data[i] - *mean, 2);
-  }
-
-  *stddev = sqrt(sum / n);
+#define CUDA_CHECK_RETURN(value)                                    \
+{                                                                   \
+  cudaError_t _m_cudaStat = value;                                  \
+  if (_m_cudaStat != cudaSuccess)                                   \
+  {                                                                 \
+    fprintf(stderr, "Error %s at line %d in file %s\n",             \
+      cudaGetErrorString(_m_cudaStat), __LINE__, __FILE__);   \
+    exit(1);                                                        \
+  }                                                                 \
 }
+
+
+
 //*****************************************************************
 // The CPU function returns a pointer to the accummulator
 void CPU_HoughTran (unsigned char *pic, int w, int h, int **acc)
@@ -72,76 +65,55 @@ void CPU_HoughTran (unsigned char *pic, int w, int h, int **acc)
       }
 }
 
-//*****************************************************************
-// TODO usar memoria constante para la tabla de senos y cosenos
-// inicializarlo en main y pasarlo al device
-//__constant__ float d_Cos[degreeBins];
-//__constant__ float d_Sin[degreeBins];
-
-//*****************************************************************
-
 
 // GPU kernel. One thread per image pixel is spawned.
 // The accummulator memory needs to be allocated by the host in global memory
 __global__ void GPU_HoughTran (unsigned char *pic, int w, int h, int *acc, float rMax, float rScale, float *d_Cos, float *d_Sin)
 {
+  // Calculo global ID
   int gloID = blockIdx.x * blockDim.x + threadIdx.x;
   if (gloID > w * h) return;      // in case of extra threads in block
 
   int xCent = w / 2;
   int yCent = h / 2;
 
+  // TODO: Explicar bien bien esta parte. Dibujar un rectangulo a modo de imagen sirve para visualizarlo mejor
+  /* R// Para calcular xCoord se debe realizar el módulo de gloID con el ancho de la imagen
+   con el fin de poder ubicar en qué posición en el eje x se encuentra nuestro hilo.
+   Luego, se realiza un corrimiento respecto a centro de la imagen.
+   Para la coordenada en y se debe divide el id de nuestro hilo por el ancho
+   esto nos ubica en el eje vertical. Luego este valor es restado respecto al centro de la image
+   y con esto se obtiene las coordenadas a partir del id global de nuestro hilo
+   que son relativas a nuestro plano
 
-  // gloID es el id global del thread y cada thread representa un pixel. El id empieza en la esquina superior izquierda
-  //  y sigue a la derecha en la línea y luego sigue con la línea de abajo.
-  //
-  // xCoord: se hace módulo con el ancho de la imágen para encontrar el pixel en x y luego se le resta xCent para
-  //  tener 0 en el centro, negativos a la izquierda y positivos a la derecha.
-  // yCoord: se divide el gloId por el ancho de la imágen para obtener el número de línea (posición en y) luego se
-  //  le restaría el yCent para hacer lo mismo que en x pero como la coordenada es al reves se "multiplica por -1"
-  //  entonces termina siendo yCent - gloId / w
+  */
   int xCoord = gloID % w - xCent;
   int yCoord = yCent - gloID / w;
 
-  //TODO eventualmente usar memoria compartida para el acumulador
-
   if (pic[gloID] > 0)
+  {
+    for (int tIdx = 0; tIdx < degreeBins; tIdx++)
     {
-      for (int tIdx = 0; tIdx < degreeBins; tIdx++)
-        {
-          //TODO utilizar memoria constante para senos y cosenos
-          //float r = xCoord * cos(tIdx) + yCoord * sin(tIdx); //probar con esto para ver diferencia en tiempo
-          float r = xCoord * d_Cos[tIdx] + yCoord * d_Sin[tIdx];
-          int rIdx = (r + rMax) / rScale;
-          //debemos usar atomic, pero que race condition hay si somos un thread por pixel? explique
-
-          // aunque es un thread por pixel el acumulador no está definido por pixeles sino que varios
-          // threads pueden accederlo por el sistema de votación, por esto se necesita atomic para evitar
-          // el race condition
-          atomicAdd (acc + (rIdx * degreeBins + tIdx), 1);
-        }
+      //float r = xCoord * cos(tIdx) + yCoord * sin(tIdx); //probar con esto para ver diferencia en tiempo
+      float r = xCoord * d_Cos[tIdx] + yCoord * d_Sin[tIdx];
+      int rIdx = (r + rMax) / rScale;
+      // TODO: Debemos usar atomic, pero que race condition hay si somos un thread por pixel? explique
+      // R// Se debe utilizar atomic ya que estamos modificando el arreglo global, el cual es compartido
+      // por todos los hilos. De esta manera garantizamos que la transacción se realice adecuadamente y no
+      // ocasione race conditions.
+      atomicAdd (acc + (rIdx * degreeBins + tIdx), 1);
     }
-
-  //TODO eventualmente cuando se tenga memoria compartida, copiar del local al global
-  //utilizar operaciones atomicas para seguridad
-  //faltara sincronizar los hilos del bloque en algunos lados
-
+  }
 }
 
 //*****************************************************************
 int main (int argc, char **argv)
 {
   int i;
-  cudaEvent_t start, stop;
-  cudaEventCreate(&start);
-  cudaEventCreate(&stop);
-
-  if (argc <= 1) {
-    printf("Please pass the filename of the image as an argument\n");
-    return -1;
-  }
 
   PGMImage inImg (argv[1]);
+  cudaEvent_t start, stop;
+  float time;
 
   int *cpuht;
   int w = inImg.x_dim;
@@ -190,54 +162,40 @@ int main (int argc, char **argv)
   // execution configuration uses a 1-D grid of 1-D blocks, each made of 256 threads
   //1 thread por pixel
   int blockNum = ceil (w * h / 256);
-  cudaEventRecord(start);
+
+  //Get time with events
+  CUDA_CHECK_RETURN( cudaEventCreate(&start) );
+  CUDA_CHECK_RETURN( cudaEventCreate(&stop) );
+  CUDA_CHECK_RETURN( cudaEventRecord(start, 0) );
+
   GPU_HoughTran <<< blockNum, 256 >>> (d_in, w, h, d_hough, rMax, rScale, d_Cos, d_Sin);
-  cudaEventRecord(stop);
+
+  CUDA_CHECK_RETURN( cudaEventRecord(stop, 0) );
+  CUDA_CHECK_RETURN( cudaEventSynchronize(stop) );
+  CUDA_CHECK_RETURN( cudaEventElapsedTime(&time, start, stop) );
 
   // get results from device
   cudaMemcpy (h_hough, d_hough, sizeof (int) * degreeBins * rBins, cudaMemcpyDeviceToHost);
 
-  bool ranSuccessfully = true;
   // compare CPU and GPU results
   for (i = 0; i < degreeBins * rBins; i++)
   {
-    if (abs(cpuht[i] - h_hough[i]) > 1){
-      ranSuccessfully = false;
+    if (cpuht[i] != h_hough[i])
       printf ("Calculation mismatch at : %i %i %i\n", i, cpuht[i], h_hough[i]);
-    }
   }
+  printf("Done!\n");
+  printf("EXEC TIME:  %3.1f ms \n", time);
 
-  float milliseconds = 0;
-  cudaEventElapsedTime(&milliseconds, start, stop);
-
-  if (ranSuccessfully) {
-    printf("Calculated Hough Transform in %f ms\n", milliseconds);
-    float mean, stddev;
-    calculateSDyMEAN(h_hough, degreeBins * rBins, &mean, &stddev);
-    std::vector<std::pair<int, int>> lines;
-    for (i = 0; i < degreeBins * rBins; i++){
-      if (h_hough[i] > (mean + 5 * stddev)) {
-        // pair order: r, th
-        int my_r = i / degreeBins;
-        int my_th = i % degreeBins;
-        std::pair<int, int> line = {my_r, my_th};
-        lines.push_back(line);
-      }
-    }
-    inImg.writeJPEGWithLines("output.jpg", lines, radInc, rBins);
-  } else
-    printf("There was a problem in the calculations :(\n");
-
-  // clean-up
-  inImg.~PGMImage();
-  cudaFree((void *) d_Cos);
-  cudaFree((void *) d_Sin);
-  cudaFree((void *) d_in);
-  cudaFree((void *) d_hough);
-  delete[]pcCos;
-  delete[]pcSin;
-  delete[]h_hough;
-  cudaDeviceReset();
+  // Clean-up
+  cudaFree ((void *) d_Cos);
+  cudaFree ((void *) d_Sin);
+  cudaFree ((void *) d_in);
+  cudaFree ((void *) d_hough);
+  free (h_hough);
+  free (cpuht);
+  free (pcCos);
+  free (pcSin);
+  cudaDeviceReset ();
 
   return 0;
 }
